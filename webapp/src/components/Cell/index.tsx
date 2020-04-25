bc07c809031c4e8515cdb38dbd1cbbbbec166d16
Aksels Ledins <aksels.ledins@gmail.com>
2020-04-25 17:22:44 +0800

    disable cell hovering if filled

--- webapp/src/components/Cell/index.tsx
+++ webapp/src/components/Cell/index.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import {PlayerMark} from "../../shared/types";
 
-const Cell = ({onClick, mark}: { mark: PlayerMark | null, onClick: Function }) => (
-  <div className="block" onClick={onClick as any}>
-    {mark === PlayerMark.CROSS && 'X'}
-    {mark === PlayerMark.CIRCLE && 'O'}
-  </div>
-)
+import './style.scss'
+
+const Cell = ({onClick, mark}: { mark: PlayerMark | null, onClick: Function }) => {
+  const specificStyle = mark === PlayerMark.CIRCLE || mark === PlayerMark.CROSS ? 'cell-disabled' : 'cell-enabled'
+
+  return (
+    <div className={`cell ${specificStyle}`} onClick={onClick as any}>
+      {mark === PlayerMark.CROSS && 'X'}
+      {mark === PlayerMark.CIRCLE && 'O'}
+    </div>
+  )
+}
 export default Cell;
