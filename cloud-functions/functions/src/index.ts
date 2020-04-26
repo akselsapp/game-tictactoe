33f350a0b6c122b36369ad646509cf9cbdc37088
Aksels Ledins <aksels.ledins@gmail.com>
2020-04-26 19:51:32 +0800

    cf: (game authority) handle tie

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -95,6 +95,7 @@ const possibleWinningCombinations = [
     [2, 0],
   ],
 ];
+
 const gameAuthority = (game: OnlineGame) => {
   if (!game.player1.id || !game.player2.id)
     return {
@@ -104,10 +105,18 @@ const gameAuthority = (game: OnlineGame) => {
 
   const l = [game.board[0].split(','), game.board[1].split(','), game.board[2].split(',')];
 
+  let moveLeft = false;
+
   const sameMark = (pc: number[][]) => {
     const a = l[pc[0][0]][pc[0][1]];
     const b = l[pc[1][0]][pc[1][1]];
     const c = l[pc[2][0]][pc[2][1]];
+
+    const notPM = (x: string) => x !== PlayerMark.CROSS && x !== PlayerMark.CIRCLE;
+
+    if (notPM(a) || notPM(b) || notPM(c)) {
+      moveLeft = true;
+    }
 
     // if we have a winning combination
     if (a === b && b === c) {
@@ -131,8 +140,15 @@ const gameAuthority = (game: OnlineGame) => {
       return sm;
     }
   }
+
+  // tie
+  if (!moveLeft) {
+    return { winner: '', status: GameStatus.END };
+  }
+
   return sm;
 };
+
 export const ticTacToe_click = functions.region('europe-west2').https.onRequest((request, response) =>
   corsHandler(request, response, async () => {
     if (!request || !request.headers || !request.headers.authorization) {
